1768132831e079d9c2363fdaa73fc8d3e66411d5
Dan Roizman <danroiz@post.bgu.ac.il>
2021-11-01 12:51:52 +0200

    fixed eureka-client bug

--- src/main/java/com/rad/ms/corona_view/access/CoronaViewAccessApplication.java
+++ src/main/java/com/rad/ms/corona_view/access/CoronaViewAccessApplication.java
@@ -1,40 +1,46 @@
-package com.rad.ms.corona_view.access;
+package com.example.eurekaclient;
 
-import java.net.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.boot.context.event.*;
-import org.springframework.context.*;
-import org.springframework.core.env.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.Environment;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CoronaViewAccessApplication implements ApplicationListener<ApplicationReadyEvent> {
 
     @Autowired
     private ApplicationContext applicationContext;
-    
-	public static void main(String[] args) {
-		SpringApplication.run(CoronaViewAccessApplication.class, args);
-	}
 
-	@Override
-	public void onApplicationEvent(ApplicationReadyEvent event)
-	{
-		try
-		{
-			String ip       = InetAddress.getLocalHost().getHostAddress();
-			String hostName = InetAddress.getLocalHost().getHostName();
-			int port        = applicationContext.getBean(Environment.class).getProperty("server.port", Integer.class, 0);
-			
-			System.out.println("*****************************************************");
-			System.out.println("* Coronaview Access is UP and Ready ");
-			System.out.println("* Host=" + hostName + ", IP=" + ip + ", Port=" + port);
-			System.out.println("*****************************************************");
-		}
-		catch (UnknownHostException e)
-		{
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaViewAccessApplication.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event)
+    {
+        try
+        {
+            String ip       = InetAddress.getLocalHost().getHostAddress();
+            String hostName = InetAddress.getLocalHost().getHostName();
+            int port        = applicationContext.getBean(Environment.class).getProperty("server.port", Integer.class, 0);
+
+            System.out.println("*****************************************************");
+            System.out.println("* Coronaview Access is UP and Ready ");
+            System.out.println("* Host=" + hostName + ", IP=" + ip + ", Port=" + port);
+            System.out.println("*****************************************************");
+        }
+        catch (UnknownHostException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
