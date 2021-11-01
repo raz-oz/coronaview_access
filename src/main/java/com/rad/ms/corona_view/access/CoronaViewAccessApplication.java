acfc459ce355a4b643b34f56def58c7f80df3ab8
Dan Roizman <danroiz@post.bgu.ac.il>
2021-11-01 13:10:25 +0200

    added discoveryclient import

--- src/main/java/com/rad/ms/corona_view/access/CoronaViewAccessApplication.java
+++ src/main/java/com/rad/ms/corona_view/access/CoronaViewAccessApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
