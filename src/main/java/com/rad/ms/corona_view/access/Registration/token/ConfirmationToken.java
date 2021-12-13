8bae04f52c45e503d52f472357c32bbce97755de
Dan Roizman <danroiz@post.bgu.ac.il>
2021-12-13 15:11:45 +0200

    fixed Id import in ConfirmationToken.java

--- src/main/java/com/rad/ms/corona_view/access/Registration/token/ConfirmationToken.java
+++ src/main/java/com/rad/ms/corona_view/access/Registration/token/ConfirmationToken.java
@@ -4,9 +4,8 @@ import com.rad.ms.corona_view.access.Entities.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
