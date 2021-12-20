2056b77b433adbebd15c4aeeab970b71dc41a5a2
Dan Roizman <danroiz@post.bgu.ac.il>
2021-12-20 11:04:34 +0200

    no need of static class anymore

--- src/main/java/com/rad/ms/corona_view/access/Security/SecurityConfig.java
+++ src/main/java/com/rad/ms/corona_view/access/Security/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.rad.ms.corona_view.access.JWT.CustomAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -18,86 +17,74 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.lang.annotation.Documented;
 import java.util.Map;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Configuration
-    public static class ConfigurationAdapter extends WebSecurityConfigurerAdapter {
-
-
-        private interface ConfigureHandler {
-            void myConfigure(HttpSecurity http) throws Exception;
-        }
-
-        enum securityType {
-            BASIC,
-            BEARER,
-            OAUTH2
-        }
-
-        Map<Integer, ConfigureHandler> configureHandlerMap  = Map.of(
-                securityType.BASIC.ordinal(), (http -> {
-                    http
-                            .csrf().disable()
-                            .formLogin().disable()
-                            .authorizeRequests()
-                            .antMatchers("/", "/registration/**", "/login")
-                            .permitAll()
-                            .anyRequest()
-                            .authenticated();
-                    http.httpBasic();
-                }),
-                securityType.BEARER.ordinal(), (http -> {
-                    http
-                            .csrf().disable()
-                            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                            .and()
-                            .authorizeRequests()
-                            .antMatchers("/", "/registration/**", "/login")
-                            .permitAll()
-                            .and()
-                           // .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-                            .addFilter(new CustomAuthenticationFilter(authenticationManagerBean()))
-                            .authorizeRequests().anyRequest().authenticated();
-                    http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-                }),
-                securityType.OAUTH2.ordinal(), (http -> {
-                    http
-                    .authorizeRequests(a -> a
-                            .antMatchers("/", "/error", "/webjars/**", "/login/oauth2", "/logout").permitAll()
-                            .anyRequest().authenticated()
-                    )
-                    .exceptionHandling(e -> e
-                            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                    )
-                    .oauth2Login();
-                })
-        );
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            configureHandlerMap.get(securityType.OAUTH2.ordinal()).myConfigure(http);
-        }
+    private interface ConfigureHandler {
+        void myConfigure(HttpSecurity http) throws Exception;
     }
 
+    enum securityType {
+        BASIC,
+        BEARER,
+        OAUTH2
+    }
+
+    private Map<Integer, ConfigureHandler> configureHandlerMap  = Map.of(
+            securityType.BASIC.ordinal(), (http -> {
+                http
+                        .csrf().disable()
+                        .formLogin().disable()
+                        .authorizeRequests()
+                        .antMatchers("/", "/registration/**", "/login")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated();
+                http.httpBasic();
+            }),
+            securityType.BEARER.ordinal(), (http -> {
+                http
+                        .csrf().disable()
+                        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        .and()
+                        .authorizeRequests()
+                        .antMatchers("/", "/registration/**", "/login")
+                        .permitAll()
+                        .and()
+                        // .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                        .addFilter(new CustomAuthenticationFilter(authenticationManagerBean()))
+                        .authorizeRequests().anyRequest().authenticated();
+                http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+            }),
+            securityType.OAUTH2.ordinal(), (http -> {
+                http
+                        .authorizeRequests(a -> a
+                                .antMatchers("/", "/error", "/webjars/**", "/login/oauth2", "/logout").permitAll()
+                                .anyRequest().authenticated()
+                        )
+                        .exceptionHandling(e -> e
+                                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                        )
+                        .oauth2Login();
+            })
+    );
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        configureHandlerMap.get(securityType.BEARER.ordinal()).myConfigure(http);
+    }
 
     @Bean
-    public AuthenticationProvider authProvider(UserDetailsService userDetailsService){
+    public AuthenticationProvider authProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder){
         DaoAuthenticationProvider provider= new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder);
         return provider;
     }
-
 
 }
