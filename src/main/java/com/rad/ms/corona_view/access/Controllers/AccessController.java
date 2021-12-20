576ef46b072a8166e1f4243054341bdda343f308
Dan Roizman <danroiz@post.bgu.ac.il>
2021-12-20 09:53:13 +0200

    added oauth2

--- src/main/java/com/rad/ms/corona_view/access/Controllers/AccessController.java
+++ src/main/java/com/rad/ms/corona_view/access/Controllers/AccessController.java
@@ -3,8 +3,15 @@ package com.rad.ms.corona_view.access.Controllers;
 import com.rad.ms.corona_view.access.Registration.IRegistrationService;
 import com.rad.ms.corona_view.access.Registration.RegistrationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class AccessController {
@@ -14,10 +21,27 @@ public class AccessController {
 
     @GetMapping(value="/")
     public String goHome(Principal user){
-        if (user!=null)
-            return ("Hello " + user.getName());
-        else
-            return "Hello guest";
+        String name = "guest";
+        if (user!=null) {
+            if (user.toString().contains("OAuth2AuthenticationToken")) {
+                OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) user;
+                OAuth2User oAuth2User = oAuth2AuthenticationToken.getPrincipal();
+                name = (String) oAuth2User.getAttributes().get("name");
+            }
+            else {
+                name = user.getName();
+            }
+        }
+        return "Hello "+name;
+    }
+
+
+    @GetMapping(value="/login/oauth2")
+    public Map<String, String> oauth2Login(){
+        Map<String,String> response = new HashMap<>();
+        response.put("login with github","http://localhost:8403/oauth2/authorization/github");
+        response.put("login with google", "http://localhost:8403/oauth2/authorization/google");
+        return response;
     }
 
     @PostMapping(value = "/registration")
@@ -28,6 +52,13 @@ public class AccessController {
     @GetMapping(path = "/registration/confirm")
     public String confirm(@RequestParam("token") String token) {
         return registrationService.confirmToken(token);
+    }
+
+    @GetMapping(path = "/login/oauth2/test")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String oauth2AuthorityTest(OAuth2AuthenticationToken token){
+        Map<String, Object> attributes =  token.getPrincipal().getAttributes();
+        return attributes.get("name") + " logged in successfully";
     }
 
 }
--- src/main/java/com/rad/ms/corona_view/access/Security/SecurityConfig.java
+++ src/main/java/com/rad/ms/corona_view/access/Security/SecurityConfig.java
@@ -3,10 +3,10 @@ package com.rad.ms.corona_view.access.Security;
 import com.rad.ms.corona_view.access.JWT.CustomAuthenticationFilter;
 import com.rad.ms.corona_view.access.JWT.CustomAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,24 +18,37 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig{
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+    enum securityType {
+        BASIC,
+        BEARER,
+        OAUTH2
+    }
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    public SecurityConfig(){
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+
+    }
+
 
     @Configuration
-    @Order(1)
+    @Order(3)
     public static class BasicWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
         protected void configure(HttpSecurity http) throws Exception {
             http
@@ -64,11 +77,33 @@ public class SecurityConfig{
                     .antMatchers("/", "/registration/**", "/login")
                     .permitAll()
                     .and()
-//                    .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                     .addFilter(new CustomAuthenticationFilter(authenticationManagerBean()))
                     .authorizeRequests().anyRequest().authenticated();
             http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
         }
+    }
+
+    @Configuration
+    @Order(1)
+    public static class OAuth2ConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    .authorizeRequests(a -> a
+                            .antMatchers("/", "/error", "/webjars/**", "/login/oauth2", "/logout").permitAll()
+                            .anyRequest().authenticated()
+                    )
+                    .exceptionHandling(e -> e
+                            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                    )
+                    .oauth2Login();
+            // @formatter:on
+        }
+
     }
 
     @Bean
